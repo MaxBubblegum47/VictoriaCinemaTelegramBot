4b894c12ae01794a6ce3fc25b725d22cf1437b3d
MaxBubblegum47 <lorenzostigliano@gmail.com>
2024-06-05 13:03:31 +0200

    Adding Comments to Unit Test

--- src/test_victoriacinemabot.py
+++ src/test_victoriacinemabot.py
@@ -1,4 +1,9 @@
-
+'''
+This test checks 3 things:
+- if the website works
+- the scraping functionality
+- if the odd messages and the even messages are available
+'''
 import pytest
 from movie import Film
 import os
@@ -8,7 +13,7 @@ class MockResponse:
     def __init__(self, content):
         self.content = content
 
-
+# Check the response of the website to see if the website works properly
 def mock_get(url):
     if "https://www.victoriacinema.it/victoria_cinema/index.php" in url:
         with open("website.html", "rb") as f:
@@ -21,18 +26,19 @@ def mock_get(url):
 def mock_requests(monkeypatch):
     monkeypatch.setattr("requests.get", mock_get)
 
-
+# Check is website.html is available, if not, that means that the
+# web_scraping method of the class Film, didn't work properly
 def test_web_scraping(mock_requests):
     Film.web_scraping()
     assert "website.html" in os.listdir()
 
-
+# Check if the messageOdd is correclty generated, otherwise there is something wrong
 def test_odd_movie(mock_requests):
     messageOdd = Film.Odd_Movie()
     assert isinstance(messageOdd, list)
     assert messageOdd
 
-
+# Check if the messageEven is correclty generated, otherwise there is something wrong
 def test_even_movie(mock_requests):
     messageEven = Film.Even_Movie()
     assert isinstance(messageEven, list)
